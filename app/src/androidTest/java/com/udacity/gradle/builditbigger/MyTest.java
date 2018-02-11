456183e41fc7317f4e8885a736b56f56148ecf74
ibrahim elfakharany <ibraheemelfakharany@gmail.com>
2018-02-11 03:23:19 +0200

    initial commit

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MyTest.java
@@ -0,0 +1,34 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by 450 G1 on 11/02/2018.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MyTest extends AndroidTestCase {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testVerifyResponse() {
+
+
+        onView(withId(R.id.instructions_text_view)).perform(click());
+        onView(withId(R.id.text_name)).check(matches(withText("why nine afraid from seven, because seven eight nine")));
+    }
+
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
@@ -18,20 +17,39 @@ import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 import java.io.IOException;
 
+import exampls.com.mylibrary.JokesActivity;
 
-class MyAsynctask extends AsyncTask<Pair<Context, String>, Void, String> {
+
+class MyAsynctask extends AsyncTask<String, Void, String> {
     private static MyApi myApiService = null;
     private Context context;
+    Listener listener = null;
+    public interface Listener{
+        void onReturn(String joke);
+    }
 
+    public MyAsynctask(Context context){
+
+        this.context = context;
+        if(context instanceof Listener ){
+            listener = (Listener) context;
+        }else {
+            try {
+                throw new Exception("the context must implement the listener");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
-    protected String doInBackground(Pair<Context, String>... params) {
+    protected String doInBackground(String... params) {
         if(myApiService == null) {  // Only do this once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("https://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
@@ -43,8 +61,8 @@ class MyAsynctask extends AsyncTask<Pair<Context, String>, Void, String> {
             myApiService = builder.build();
         }
 
-        context = params[0].first;
-        String name = params[0].second;
+
+        String name = params[0];
 
         try {
             return myApiService.sayHi(name).execute().getJoke();
@@ -55,10 +73,10 @@ class MyAsynctask extends AsyncTask<Pair<Context, String>, Void, String> {
 
     @Override
     protected void onPostExecute(String joke) {
-        Toast.makeText(context, joke, Toast.LENGTH_SHORT).show();
+        listener.onReturn(joke);
     }
 }
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyAsynctask.Listener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,16 +113,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        new MyAsynctask().execute(new Pair<Context, String>(this,"Joke" ));
 
-        /*myClass myClass = new myClass();
-        String joke =  myClass.getJoke();
-        Toast.makeText(this, joke , Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, JokesActivity.class);
-        intent.putExtra("joke", joke);
-        startActivity(intent);*/
+        MyAsynctask myAsynctask = new MyAsynctask(this);
+        myAsynctask.execute("why nine afraid from seven, because seven eight nine" );
 
     }
 
 
+    @Override
+    public void onReturn(String joke) {
+        Intent intent = new Intent(this, JokesActivity.class);
+        intent.putExtra("joke", joke);
+        startActivity(intent);
+    }
 }
